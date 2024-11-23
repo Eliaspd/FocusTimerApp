22aad96b6c9e7a3c7c537347c9e68f4824cd3b08
Eliaspd <swedetailer@gmail.com>
2024-11-23 13:59:40 +0100

    Created UI for Sign Up page

--- FocusTimerApp/ContentView.swift
+++ FocusTimerApp/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
         ZStack {
             Color.black
@@ -19,8 +22,65 @@ struct ContentView: View {
                 .offset(y: -350)
             
             VStack(spacing: 20) {
+                Text("Welcome")
+                    .foregroundColor(.white)
+                    .font(.system(size: 40, weight: .bold, design: .rounded))
+                    .offset(x: -100, y: -100)
                 
+                TextField("Email", text: $email)
+                    .foregroundColor(.white)
+                    .textFieldStyle(.plain)
+                    .placeholder(when: email.isEmpty) {
+                        Text("Email")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                Rectangle()
+                    .frame(width: 350, height: 1)
+                    .foregroundColor(.white)
+                
+                SecureField("Password", text: $password)
+                    .foregroundColor(.white)
+                    .textFieldStyle(.plain)
+                    .placeholder(when: password.isEmpty) {
+                        Text("Password")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                
+                Rectangle()
+                    .frame(width: 350, height: 1)
+                    .foregroundColor(.white)
+                
+                Button {
+                    //Sign Up
+                } label: {
+                    Text("Sign up")
+                        .bold()
+                        .frame(width: 200, height: 40)
+                        .background {
+                            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                .fill(.linearGradient(colors: [.purple, .blue], startPoint: .top, endPoint: .bottomTrailing))
+                        }
+                        .foregroundColor(.white)
+                }
+                .padding(.top)
+                .offset(y: 100)
+                
+                Button {
+                    //Login
+                } label: {
+                    Text("Already have an account? Login")
+                        .bold()
+                        .foregroundColor(.white)
+                
+                }
+                .padding(.top)
+                .offset(y: 110)
+
+
             }
+            .frame(width: 350)
         }
         .ignoresSafeArea()
         }
@@ -29,4 +89,16 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+extension View {
+    func placeholder<Content: View>(
+        when shouldShow: Bool,
+        alignment: Alignment = .leading,
+        @ViewBuilder placeholder: () -> Content) -> some View {
+
+        ZStack(alignment: alignment) {
+            placeholder().opacity(shouldShow ? 1 : 0)
+            self
+        }
+    }
 }
