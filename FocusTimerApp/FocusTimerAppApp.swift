5cc94f2bef3776076b3abb0130bbdc9bff7991dd
Eliaspd <swedetailer@gmail.com>
2024-11-23 12:25:16 +0100

    First Commit

---
+++ FocusTimerApp/FocusTimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimerAppApp.swift
+//  FocusTimerApp
+//
+//  Created by Elias Puolitaival on 2024-11-21.
+//
+
+import SwiftUI
+
+@main
+struct FocusTimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FocusTimerAppTests/FocusTimerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimerAppTests.swift
+//  FocusTimerAppTests
+//
+//  Created by Elias Puolitaival on 2024-11-21.
+//
+
+import Testing
+@testable import FocusTimerApp
+
+struct FocusTimerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
